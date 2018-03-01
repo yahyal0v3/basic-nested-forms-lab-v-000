1ecf0e00c0fcd6f52f09c2d360f1ac49ca0878bf
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-01 17:55:21 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    raise @recipe.ingredients.inspect
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    ingredients: [
+      :name,
+      :quantity
+      ])
   end
 end
