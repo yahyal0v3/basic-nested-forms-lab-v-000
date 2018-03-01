3b1e6c6b2a9cb8786133c296d30d904c36061cdf
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-01 18:15:50 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,6 +14,7 @@ class RecipesController < ApplicationController
   end
 
   def create
+    raise params.inspect
     @recipe = Recipe.create(recipe_params)
     redirect_to recipe_path(@recipe)
   end
